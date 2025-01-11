dc4a1642f0c3f6d6f7059cd3ebac9a03f5099506
Dima Monid <1234567waka.waka@gmail.com>
2025-01-11 20:18:10 +0300

    TrafficLight

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lightViewRed: UIView!
+    @IBOutlet var lightViewOrange: UIView!
+    @IBOutlet var lightViewGreen: UIView!
+    @IBOutlet var startButton: UIButton!
+    private var currentLight: CurrentLight = .red
+    private let lightOn = 1.0
+    private let lightOff = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lightViewRed.layer.cornerRadius = 50
+        lightViewRed.alpha = lightOff
+        lightViewOrange.layer.cornerRadius = 50
+        lightViewOrange.alpha = lightOff
+        lightViewGreen.layer.cornerRadius = 50
+        lightViewGreen.alpha = lightOff
+        
+
     }
 
+    @IBAction func startButtonDidTapped(_ sender: UIButton) {
+        switch currentLight{
+        case .red:
+            lightViewGreen.alpha = lightOff
+            lightViewRed.alpha = lightOn
+            currentLight = .orange
+        case .orange:
+            lightViewRed.alpha = lightOff
+            lightViewOrange.alpha = lightOn
+            currentLight = .green
+        case .green:
+            lightViewOrange.alpha = lightOff
+            lightViewGreen.alpha = lightOn
+            currentLight = .red
+        }
+        sender.setTitle("NEXT", for: .normal)
 
+    }
+    
 }
 
+extension ViewController {
+    private enum CurrentLight {
+        case red, orange, green
+    }
+}
